f646c1e7d5aa6536a146e867ab4da4cd733b7ba4
mithilgupta <mithilgupta@gmail.com>
2024-09-15 19:05:58 -0400

    V6

--- src/.ipynb_checkpoints/lambda_function-checkpoint.py
+++ src/.ipynb_checkpoints/lambda_function-checkpoint.py
@@ -1,8 +1,13 @@
+# GitHub Actions taken from : https://www.youtube.com/watch?v=xs9oJaaU8I8&t=265s
+
 import json
 
 def lambda_handler(event, context):
     # TODO implement
+
+	input_prompt=event['prompt']
+    print(input_prompt)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda_v5!')
+        'body': json.dumps(input_prompt)
     }
