56be6a5a4c815e5980692a6900bcf1dd066f4fcc
mithilgupta <mithilgupta@gmail.com>
2024-09-15 17:52:27 -0400

    V5

--- src/.ipynb_checkpoints/lambda_function-checkpoint.py
+++ src/.ipynb_checkpoints/lambda_function-checkpoint.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda_v4!')
+        'body': json.dumps('Hello from Lambda_v5!')
     }
